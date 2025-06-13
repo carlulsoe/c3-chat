ae5bfded10a988bb0f3fff59962d3ff27a0f21e1
carlulsoe <34673973+carlulsoe@users.noreply.github.com>
2025-06-13 18:50:46 +0200

    Rename content

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,12 @@ export default function Home() {
   return (
     <>
       <header className="sticky top-0 z-10 bg-background p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
-        Convex + Next.js + Clerk
+        C3 Chat
         <UserButton />
       </header>
       <main className="p-8 flex flex-col gap-8">
         <h1 className="text-4xl font-bold text-center">
-          Convex + Next.js + Clerk
+          C3 Chat
         </h1>
         <Authenticated>
           <Content />
