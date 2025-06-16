014aa337710b7ee9c9e6e49ca47af25ee6206f49
carlulsoe <34673973+carlulsoe@users.noreply.github.com>
2025-06-16 21:00:04 +0200

    More models

--- lib/models.ts
+++ lib/models.ts
@@ -11,4 +11,20 @@ export const models = [
     model: "deepseek/deepseek-r1-0528-qwen3-8b:free",
     name: "DeepSeek R1 8b",
   },
+  {
+    model: "openai/o4-mini",
+    name: "o4-mini",
+  },
+  {
+    model: "openai/o3",
+    name: "o3",
+  },
+  {
+    model: "google/gemini-2.5-pro-preview",
+    name: "Gemini 2.5 Pro",
+  },
+  {
+    model: "anthropic/claude-sonnet-4",
+    name: "Claude Sonnet 4",
+  },
 ];
