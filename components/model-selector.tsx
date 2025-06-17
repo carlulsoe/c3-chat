5417872120f7e6c1f5e8991ad46ca5a4033b6342
carlulsoe <34673973+carlulsoe@users.noreply.github.com>
2025-06-17 17:12:11 +0200

    Switch default model

--- components/model-selector.tsx
+++ components/model-selector.tsx
@@ -32,7 +32,7 @@ const getProviderName = (model: string) => {
     return 'Google'
 }
 
-export const defaultModel: Model = models.find((model) => model.model === "gemini-2.0-flash")!
+export const defaultModel: Model = models.find((model) => model.model === "gemini-2.5-flash-preview-04-17")!
 
 export function ModelSelector({ selectedModel, setSelectedModel }: ModelSelectorProps) {
     return (
